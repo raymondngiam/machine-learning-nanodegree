f81dfdd7139d7fa3a11aca143f26832b8fdd2e45
Raymond Ngiam <ha.ngiam87@gmail.com>
2016-09-24 21:51:13 +0800

    Implement exploration rate

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -80,9 +80,12 @@ class LearningAgent(Agent):
         self.state = (inputs['light'], inputs['oncoming'],inputs['left'],self.next_waypoint)
         
         # TODO: Select action according to your policy
-        #actions =  [None,'forward','left','right']
-        #action = random.choice(actions)
-        action = self.computeActionFromQValues(self.state)
+        actions =  [None,'forward','left','right']
+        bolExplore = self.flipCoin(self.epsilon)
+        if (bolExplore):
+            action = random.choice(actions)
+        else:
+            action = self.computeActionFromQValues(self.state)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
